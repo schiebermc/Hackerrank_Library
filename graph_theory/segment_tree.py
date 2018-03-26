86521012abd0a7ad87b4ddc8794b5953016b734a
schiebermc <matthewschieber@gmail.com>
2018-03-26 14:54:28 -0400

    updates segment tree

--- graph_theory/segment_tree.py
+++ graph_theory/segment_tree.py
@@ -1,5 +1,14 @@
 
+import sys
+
 class SegmentTree(object):
+    
+    # this class was constructed with inspiration and guidance from:
+    # https://leetcode.com/articles/a-recursive-approach-to-segment-trees-range-sum-queries-lazy-propagation/
+    # O(n) leaves, O(n-1) internal nodes.
+    # build:  O(n) :D
+    # update: O(k), where k = height of tree :D
+    # query:  O(k), where k = height of tree :D
     
     def __init__(self, n):
         
@@ -35,4 +44,46 @@ class SegmentTree(object):
         
         if(self.merge_op == '+'):
             return val1 + val2
+        
+        
+    def query(self, ind, lo, hi, i, j):
+        # call as query(0, 0, n-1, i, j)
+        # where i:j is the slice being queried
+        
+        if (lo > j or hi < i):
+            return 0
+        
+        if (i <= lo and j >= hi):
+            return self.tree[ind]
+        
+        mid = lo + (hi - lo) // 2
+        
+        if (i > mid):
+            return self.query(2 * ind + 2, mid + 1, hi, i, j)
+        elif (j <= mid):
+            return self.query(2 * ind + 1, lo, mid, i, j)
+        
+        lq = self.query(2 * ind + 1, lo, mid, i, mid)
+        rq = self.query(2 * ind + 2, mid + 1, hi, mid + 1, j)
+    
+        return self.merge(lq, rq)
+    
+    def update_value(self, tind, lo, hi, aind, val):
+        # call as update_value(0, 0, n-1, i, val)
+        # updates value of array index with val
+        
+        if (lo = hi):
+            tree[tind] = val
+            return
+        
+        mid = lo + (hi - lo) // 2 
+        
+        if (aind > mid):
+            self.update_value(2 * tind + 2, mid + 1, hi, aind, val)
+        elif (aind <= mid):
+            self.update_value(2 * tind + 1, lo, mid, aind, val)
+            
+        self.tree[tind] = self.merge(self.tree[2 * tind + 1], self.tree[2 * tind + 2])
+
+
 
